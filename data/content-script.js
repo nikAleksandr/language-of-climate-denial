d7418d7fdd66a8db05b177362ec83f76d63227a7
Nick Lyell <njlyell@gmail.com>
2016-01-03 21:51:28 -0700

    added climate terms for regex practice

--- data/content-script.js
+++ data/content-script.js
@@ -17,225 +17,75 @@ function walk(rootNode)
 
 function handleText(textNode) {
     var text = textNode.wholeText;
-    if(text.search(/millennial/i) >= 0
-        || text.search(/millenial/i) >= 0 // misspelling
-        || text.search(/(mil·len·ni·al|mə-lĕn′ē-əl)/i) >= 0
-        || text.search(/miˈlenēəl/i) >= 0
-        || text.search(/great (depression|recession)/i) >= 0
-        || text.search(/(ows|occupy) movement/i) >= 0
-        || text.search(/helicopter parent/i) >= 0
-        || text.search(/trophy kid/i) >= 0
-        || text.search(/digital native/i) >= 0
-        || text.search(/generation (z|y|we|me|global|next|net|flux|sell|boomerang|peter pan|9\/?11|precarious)/i) >= 0
-        || text.search(/(z|y|we|me|global|next|net|flux|sell|boomerang|peter pan|9\/?11|precarious) generation/i) >= 0
-        || text.search(/tween/i) >= 0
-        || text.search(/Mileurista/i) >= 0) {
+    if(text.search(/oil/i) >= 0
+        || text.search(/coal/i) >= 0
+        || text.search(/gas/i) >= 0
+        || text.search(/gasoline/i) >= 0
+        || text.search(/industry/i) >= 0 //always change industry, even when oil, gas, etc, aren't mentioned
+        || text.search(/(all-of-the-above|all of the above)/i) >= 0
+        || text.search(/offshore (platform|rig|oil rig|drilling|oil rig|extraction|oil extraction|production|oil production)/i) >= 0
+        || text.search(/(fracking|hydraulic fracturing|fraking)/i) >= 0 //common misspellings?
+        || text.search(/(tar sands|tar-sands)/i) >= 0
+        || text.search(/deforest/i) >= 0
+        || text.search(/(wild fire|forest fire)/i) >= 0
+        || text.search(/drought/i) >= 0
+        || text.search(/dispersant/i) >= 0
+        || text.search(/flood/i) >= 0
+        || text.search(/(super storm|superstorm|super-storm)/i) >= 0
+        || text.search(/refinery/i) >= 0
+        || text.search(/pipeline/i) >= 0
+        || text.search(/(chemichal|petrochemical) plant/i) >= 0
+        || text.search(/refinery/i) >= 0
+        || text.search(/greenhouse gas/i) >= 0
+        || text.search(/(mainstream|corporate) media/i) >= 0
+        || text.search(/(big oil|OPEC)/i) >= 0) {
         textNode.nodeValue = replaceText(textNode.nodeValue);
     }
 }
 
 function replaceText(v)
 {
-    // Fix some misspellings
-    v = v.replace(/\b(M|m)illienial(s)?\b/g, "$1illennial$2");
-    v = v.replace(/\b(M|m)illenial(s)?\b/g, "$1illennial$2");
-    v = v.replace(/\b(M|m)ilennial(s)?\b/g, "$1illennial$2");
-    v = v.replace(/\b(M|m)ilenial(s)?\b/g, "$1illennial$2");
-
-    // Millennial Generation
+    // all of the above
     v = v.replace(
-        /\b(?:Millennial Generation)|(?:Generation Millennial)\b/g,
-        "Plissken Faction"
+        /\b(?:All-[Oo]f-[Tt]he-Above|All-of-the-above|All [Oo]f [Tt]he Above|All of the above)\b/g,
+        "Carbon Paganistic"
     );
     v = v.replace(
-        /\b(?:millennial generation)|(?:generation millennial)\b/g,
-        "Plissken faction"
+        /\b(?:all-of-the-above|all of the above)\b/g,
+        "carbon paganistic"
     );
 
-    // Millennialism
-    v = v.replace(/\bMillennialism\b/g, "Reptilianism");
-    v = v.replace(/\bmillennialism\b/g, "reptilianism");
+    // natural, all-natural, clean, eco-friendly, green
+    v = v.replace(/\b(?:Natural|All-[Nn]atural|Green|Clean|Eco-[Ff]riendly|Ecofriendly|Green)\b/g, "Enchanted");
+    v = v.replace(/\b(?:natural|all-natural|green|clean|eco-friendly|ecofriendly|green)\b/g, "enchanted");
 
+    //drilling;
+    v = v.replace(/\bdrill(?:(ing)|(s)|(ed))?\b/g, "Terradeform$1$2$3");
+    v = v.replace(/\bdrill(?:(ing)|(s)|(ed))?\b/g, "terradeform$1$2$3");
 
-    //  Gendered Millennials
-    v = v.replace(/\bMillennial (M|m)(e|a)n('s)?\b/g, "Simone $1$2n$3");
-    v = v.replace(/\bmillennial m(e|a)n('s)?\b/g, "Simone m$1n$2");
-    v = v.replace(/\bMillennial (B|b)oy('s|s(?:')?)?\b/g, "Simone $1oy$2");
-    v = v.replace(/\bmillennial boy('s|s(?:')?)?\b/g, "Simone boy$1");
-    v = v.replace(/\bMillennial (G|g)uy('s|s(?:')?)?\b/g, "Simone $1uy$2");
-    v = v.replace(/\bmillennial guy('s|s(?:')?)?\b/g, "Simone guy$1");
-    v = v.replace(/\bMillennial (W|w)om(e|a)n('s)?\b/g, "Simone $1om$2n$3");
-    v = v.replace(/\bmillennial wom(e|a)n('s)?\b/g, "Simone wom$1n$2");
-    v = v.replace(/\bMillennial (G|g)irl('s|s(?:')?)?\b/g, "Simone $1irl$2");
-    v = v.replace(/\bmillennial girl('s|s(?:')?)?\b/g, "Simone girl$1");
-    v = v.replace(/\bMillennial (G|g)al('s|s(?:')?)?\b/g, "Simone $1al$2");
-    v = v.replace(/\bmillennial gal('s|s(?:')?)?\b/g, "Simone gal$1");
+    //offshore, deepwater
+    v = v.replace(/\bOffshore\b/g, "Subsea");
+    v = v.replace(/\boffshore\b/g, "subsea");
 
-    //  Aged Millennials
-    v = v.replace(/\bMillennial Child('s)?\b/g, "Simonelet$1");
-    v = v.replace(/\b[Mm]illennial child('s)?\b/g, "Simonelet$1");
-    v = v.replace(/\bMillennial Children(?:(')s)?\b/g, "Simonelets$1");
-    v = v.replace(/\b[Mm]illennial children(?:(')s)?\b/g, "Simonelets$1");
-    v = v.replace(
-        /\bMillennial [Tt]een(?:ager)?('s)?\b/g,
-        "proto-Simone Person$1"
-    );
-    v = v.replace(/\bmillennial teen(?:ager)?('s)?\b/g, "proto-Simone person$1");
-    v = v.replace(
-        /\bMillennial [Tt]een(?:ager)?(?:(s)\b(')|s\b)/g,
-        "proto-Simone People$2$1"
-    );
-    v = v.replace(
-        /\bmillennial teen(?:ager)?(?:(s)\b(')|s\b)/g,
-        "proto-Simone people$2$1"
-    );
-    v = v.replace(/\bMillennial (A|a)dult('s)?\b/g, "$1dult Simone Person$2");
-    v = v.replace(/\bmillennial adult('s)?\b/g, "adult Simone person$1");
-    v = v.replace(
-        /\bMillennial (A|a)dult(?:(s)\b(')|s\b)/g,
-        "$1dult Simone People$3$2"
-    );
-    v = v.replace(
-        /\bmillennial adult(?:(s)\b(')|s\b)/g,
-        "adult Simone people$2$1"
-    );
+    //underground
+    v = v.replace(/\bUnderground\b/g, "Subterranean");
+    v = v.replace(/\bunderground\b/g, "subterranean");
 
-     // Definition
-    v = v.replace(/\bmil·len·ni·al\b/g, "sn·a·ke pe·op·le");
-    v = v.replace(/\bmə-lĕn′ē-əl\b/g, "snāk ˈpēpəl");
+    //spill
+    v = v.replace(/\bSpill\b/g, "Valdezian Gush");
+    v = v.replace(/\bspill\b/g, "valdezian gush");
 
-    // Millennial
-    v = v.replace(/\bMillennial\b/g, "Simone Person");
-    v = v.replace(/\bmillennial\b/g, "Simone person");
-    v = v.replace(/\bMillennial(?:(s)\b(')|s\b)/g, "Simone People$2$1");
-    v = v.replace(/\bmillennial(?:(s)\b(')|s\b)/g, "Simone people$2$1");
+    //tar sands
+    v = v.replace(/\b(?:Tar-[Ss]ands|Tar [Ss]ands|Tarsands|Oil-[Ss]ands|Oil [Ss]ands)(?: [Oo]peration)?\b/g, "Moon-scaping Operation");
+    v = v.replace(/\b(?:tar-sands|tar sands|tarsands|oil-sands|oil sands)(?: [Oo]peration)?\b/g, "Moon-scaping Operation");
 
-    // The Great Recession
-    v = v.replace(/\bGreat Recession\b/g, "Time of Shedding and Cold Rocks");
-    v = v.replace(/\bgreat recession\b/g, "time of shedding and cold rocks");
-
-    // The Great Depression
-    v = v.replace(/\bGreat Depression\b/g, "Clutch Plague");
-    v = v.replace(/\bgreat depression\b/g, "clutch plague");
-
-    // Occupy Wall Street
-    v = v.replace(
-        /\b(?:(?:Occupy|OWS) (?:M|m)ovement)|(?:Occupy Wall Street)\b/g,
-        "Great Ape-Simone War"
-    );
-    v = v.replace(
-        /\b(?:(?:occupy|OWS|ows) movement)|(?:occupy wall street)\b/g,
-        "great ape-Simone war"
-    );
-    v = v.replace(/\bOWS\b/g, "GA-SW");
-    v = v.replace(/\bows\b/g, "ga-sw");
-
-    // Helicopter Parents
-    v = v.replace(/\bHelicopter Parent(?:(s)\b(')|s\b)/g, "Thulsa Doom$2$1");
-    v = v.replace(/\b[Hh]elicopter parent(?:(s)\b(')|s\b)/g, "Thoth-Amon$2$1");
-
-    // Trophy Kids
-    v = v.replace(/\bTrophy Kid(?:(s)\b(')|s\b)/g, "Quetzalcoatl's Chosen$2$1");
-    v = v.replace(/\btrophy kid(?:(s)\b(')|s\b)/g, "Quetzalcoatl's chosen$2$1");
-
-    // Digital Natives
-    v = v.replace(/\bDigital Native(s)?\b/g, "Parseltongue$1");
-    v = v.replace(/\bdigital native(s)?\b/g, "parseltongue$1");
-
-    // Generation Y
-    v = v.replace(/\b(?:Generation Y)|(?:Generation Why)\b/g,
-        "Serpent Society");
-    v = v.replace(/\bgen(?:eration)? ?(?:wh)?y\b/g, "serpent society");
-    v = v.replace(/\bGen Y\b/g, "Society of the Serpent");
-
-    // Generation We
-    v = v.replace(/\bGeneration We\b/g, "Caduceus Cult");
-    v = v.replace(/\bgeneration we\b/g, "caduceus cult");
-    v = v.replace(/\bWe Generation\b/g, "Cult of the Caduceus");
-    v = v.replace(/\bwe generation\b/g, "cult of the caduceus");
-
-    // Generation Me
-    v = v.replace(/\bGeneration Me\b/g, "The Cult of the Serpent");
-    v = v.replace(/\bgeneration me\b/g, "the cult of the serpent");
-
-    // Global Generation
-    v = v.replace(/\bGlobal Generation\b/g, "Tannin's Horde");
-    v = v.replace(/\bglobal generation\b/g, "Tannin's horde");
-    v = v.replace(/\bGeneration Global\b/g, "Horde of Tannin");
-    v = v.replace(/\bgeneration global\b/g, "horde of Tannin");
-
-    // Generation Next
-    v = v.replace(/\bGeneration Next\b/g, "Time of Nidhogg");
-    v = v.replace(/\bgeneration next\b/g, "time of Nidhogg");
-
-    // Net Generation
-    v = v.replace(/\bNet Generation\b/g, "Damballa's Coils");
-    v = v.replace(/\bnet generation\b/g, "Damballa's coils");
-    v = v.replace(/\bGeneration Net\b/g, "Coils of Damballa");
-    v = v.replace(/\bgeneration net\b/g, "Coils of Damballa");
-
-    // Echo Boomers
-    v = v.replace(/\bEcho Boomers\b/g, "Crotalids");
-    v = v.replace(/\becho Boomers\b/g, "crotalids");
-
-    // New Boomers
-    v = v.replace(/\bNew Boomer(?:(s)\b(')|s\b)/g, "Jörmungandr's Circle$2$1");
-    v = v.replace(/\bnew Boomer(?:(s)\b(')|s\b)/g, "Jörmungandr's circle$2$1");
-
-    // Generation Flux
-    v = v.replace(/\b(?:Generation Flux)|(?:Flux Generation)\b/g, "Hiss Club");
-    v = v.replace(/\b(?:generation flux)|(?:flux generation)\b/g, "hiss club");
-
-    // Generation Sell
-    v = v.replace(/\bGeneration Sell\b/g, "Kaa Tribe");
-    v = v.replace(/\bgeneration sell\b/g, "Kaa tribe");
-    v = v.replace(/\bSell Generation\b/g, "Tribe of Kaa");
-    v = v.replace(/\bsell generation\b/g, "tribe of Kaa");
-
-    // Boomerang Generation
-    v = v.replace(
-        /\b(?:Boomerang Generation)|(?:Generation Boomerang)\b/g,
-        "Ouroboros Society"
-    );
-    v = v.replace(
-        /\b(?:boomerang generation)|(?:generation boomerang)\b/g,
-        "ouroboros society"
-    );
-
-    // Peter Pan Generation
-    v = v.replace(/\bPeter Pan Generation\b/g, "Neheb-Kau Cult");
-    v = v.replace(/\b(?:P|p)eter (?:P|p)an generation\b/g, "Neheb-Kau cult");
-    v = v.replace(/\bGeneration Peter Pan\b/g, "Cult of Neheb-Kau");
-    v = v.replace(/\bgeneration (?:P|p)eter (?:P|p)an\b/g, "cult of Neheb-Kau");
-
-    // Generation 911
-    v = v.replace(/\bGen(?:eration)? 9\/?11\b/g, "Kaa Tribe");
-    v = v.replace(/\bgen(?:eration)? 9\/?11\b/g, "Kaa tribe");
-    v = v.replace(/\b9\/?11 Generation\b/g, "Tribe of the Kaa");
-    v = v.replace(/\b9\/?11 generation\b/g, "tribe of the Kaa");
-
-    // The Generation of €700
-    v = v.replace(
-        /\b(?:The Generation of €700)|(?:€700 Generation)\b/g,
-        "Ophion"
-    );
-    v = v.replace(
-        /\b(?:the generation of €700)|(?:€700 generation)\b/g,
-        "ophion"
-    );
-
-    // Mileurista
-    v = v.replace(/\b(?:M|m)ill?eurista\b/g, "Nagual");
-
-    // Precarious Generation
-    v = v.replace(
-        /\b(?:Precarious Generation)|(?:Generation Precarious)\b/g,
-        "Gargouille"
-    );
-    v = v.replace(
-        /\b(?:precarious generation)|(?:generation precarious)\b/g,
-        "gargouille"
-    );
+    //climate change and it's disasters
+    v = v.replace(/\b(?:Wildfire|Forest Fire)\b/g, "Entropic Beacon");
+    v = v.replace(/\b(?:wildfire|forest fire)\b/g, "entropic beacon");
+    v = v.replace(/\bDrought(s)?\b/g, "Drydeath$1");
+    v = v.replace(/\bdrought(s)?\b/g, "drydeath$1");
+    v = v.replace(/\bFlood(?:(ing)|(s)|(ed))?\b/g, "Wetdeath$1$2$3");
+    v = v.replace(/\bflood(?:(ing)|(s)|(ed))?\b/g, "wetdeath$1$2$3");
 
     return v;
 }
